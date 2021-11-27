d8f4ad61d9a6b3871b6577c2caaa843efa0c2e63
Carlos Eduardo Cabrera Ardila <ccabreraa@unal.edu.co>
2021-11-26 21:54:55 -0500

    Iniciando_Proyecto
    Iniciando proyecto React

---
+++ src/App.js
@@ -0,0 +1,17 @@
+//library React
+import React from "react"
+//Apps
+
+class App extends React.Component{
+
+    render(){
+        return(
+            <div>
+                "Hola mundo"
+            </div>
+        )
+    }
+
+}
+
+export default App;
