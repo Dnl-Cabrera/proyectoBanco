ac76b231eba7d41f5cf5bd2dd1bac9acf355adfe
Carlos Eduardo Cabrera Ardila <ccabreraa@unal.edu.co>
2021-12-13 22:24:00 -0500

    Actualizacion proyecto
    Actualizacion todas las vistas

--- front/src/App.js
+++ front/src/App.js
@@ -5,6 +5,7 @@ import React from "react"
 import InicioAplicacion from "./pages/InicioAplicacion/"
 
 //Pages administrador
+import AdminAprobarSolicitud from "./pages/Administrador/AdminAprobarSolicitud"
 import AdminInicio from "./pages/Administrador/AdminInicio/AdminInicio";
 import NavAdministrador from "./components/Administrador/NavAdministrador/NavAdministrador"
 import AdminModificarCredito from "./pages/Administrador/AdminModificarCredito/AdminModificarCredito"
@@ -14,6 +15,12 @@ import VisualizarUser from "./pages/Administrador/VisualizarUser/VisualizarUser"
 
 //Paginas Cliente
 import ClienteAdministrador from "./pages/Clientes/ClienteAdministrador/ClienteAdministrador"
+import ClienteCertificado from "./pages/Clientes/ClienteCertificado"
+import ClienteSolicitudCredito from "./pages/Clientes/ClienteSolicitudCredito"
+import ClienteSolicitudProrroga from "./pages/Clientes/ClienteSolicitudProrroga"
+
+//
+import Boton from "./components/Cliente/Boton"
 
 //Pagina Registro
 import Registro from "./pages/Registro";
@@ -23,7 +30,7 @@ class App extends React.Component{
     render(){
         return(
             <div>
-                    <Registro/>
+                    <AdminAprobarSolicitud/>
             </div>
         )
     }
